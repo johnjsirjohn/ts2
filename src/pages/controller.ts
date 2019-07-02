a12a511505ce6dcc80ac8920203ce72ab9b16a5c
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-07-02 15:37:11 +0200

    Adding a connection to postgres db

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,42 +1,49 @@
 import {
   JsonController,
   Get,
-  Param,
-  Put,
-  Body,
-  Post,
-  HttpCode
+  Param
+  // Put,
+  // Body,
+  // Post,
+  // HttpCode
 } from 'routing-controllers';
-import pagesById, { Page } from './data';
-
+// import pagesById, { Page } from './data';
+import Page from './entity';
 //type PageList = { pages: Page[] }; is the same as interface PageList {  pages: Page[];}
-
-interface PageList {
-  pages: Page[];
-}
 
 @JsonController()
 export default class PageController {
   @Get('/pages/:id')
-  getPage(@Param('id') id: number): Page {
-    return pagesById[id];
+  getPage(@Param('id') id: number) {
+    return Page.findOne(id);
   }
-
   @Get('/pages')
-  allPages(): PageList {
-    return { pages: Object.values(pagesById) };
-  }
-
-  @Put('/pages/:id')
-  updatePage(@Param('id') id: number, @Body() body: Partial<Page>): Page {
-    console.log(`Incoming PUT body param:`, body);
-    return pagesById[id];
-  }
-
-  @Post('/pages')
-  @HttpCode(201)
-  createPage(@Body() body: Page): Page {
-    console.log(`Incoming POST body param:`, body);
-    return body;
+  async allPages() {
+    return await Page.find();
   }
 }
+// import pagesById, { Page } from './data';
+//type PageList = { pages: Page[] }; is the same as interface PageList {  pages: Page[];}
+// @Get('/pages/:id')
+// getPage(@Param('id') id: number): Page {
+//   return pagesById[id];
+// }
+
+// @Get('/pages')
+// allPages(): PageList {
+//   return { pages: Object.values(id) };
+// }
+
+// @Put('/pages/:id')
+// updatePage(@Param('id') id: number, @Body() body: Partial<Page>): Page {
+//   console.log(`Incoming PUT body param:`, body);
+//   return pagesById[id];
+// }
+
+// @Post('/pages')
+// @HttpCode(201)
+// createPage(@Body() body: Page): Page {
+//   console.log(`Incoming POST body param:`, body);
+//   return body;
+// }
+//}
