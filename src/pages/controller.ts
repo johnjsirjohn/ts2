40f9fc36be58aebc493cd3a87a9c84cffac41467
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-07-02 15:50:09 +0200

    Added Create/Edit a Page

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,11 +1,12 @@
 import {
   JsonController,
   Get,
-  Param
-  // Put,
-  // Body,
-  // Post,
-  // HttpCode
+  Param,
+  Put,
+  Body,
+  NotFoundError,
+  Post,
+  HttpCode
 } from 'routing-controllers';
 // import pagesById, { Page } from './data';
 import Page from './entity';
@@ -20,6 +21,18 @@ export default class PageController {
   @Get('/pages')
   async allPages() {
     return await Page.find();
+  }
+  @Put('/pages/:id')
+  async updatePage(@Param('id') id: number, @Body() update: Partial<Page>) {
+    const page = await Page.findOne(id);
+    if (!page) throw new NotFoundError('Cannot find page');
+
+    return Page.merge(page, update).save();
+  }
+  @Post('/pages')
+  @HttpCode(201)
+  createPage(@Body() page: Page) {
+    return page.save();
   }
 }
 // import pagesById, { Page } from './data';
