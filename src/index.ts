4752b0b5893af29fa7bef2b3612a2118e467f666
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-07-02 16:43:48 +0200

    Create, read, update users

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import 'reflect-metadata';
 import { createKoaServer } from 'routing-controllers';
 import PageController from './pages/controller';
+import UserController from './users/controller';
 import setupDb from './db';
 
 const port = process.env.PORT || 4000;
 
 const app = createKoaServer({
-  controllers: [PageController]
+  controllers: [PageController, UserController]
 });
 
 setupDb()
